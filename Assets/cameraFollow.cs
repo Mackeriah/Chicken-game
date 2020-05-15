336ef6ffba3a2c496083e7f62490579b155fab06
Owen <fitzgerald.owen@gmail.com>
2020-05-15 22:49:23 +1000

    camera follows player v2

--- Assets/cameraFollow.cs
+++ Assets/cameraFollow.cs
@@ -9,11 +9,15 @@ public class cameraFollow : MonoBehaviour
     // offset camera position
     public Vector3 offset;
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
         // follow target and add offset
         // this is added a vector3 (target position) to a vector3 (camera offset position) so it literally adds the x+x, y+y and z+z
-        transform.position = target.position + offset; 
+        Vector3 desiredPosition = target.position + offset;
+        Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.position = smoothedPosition;
+
+        transform.LookAt(target);
     }
 
-}
+} 
