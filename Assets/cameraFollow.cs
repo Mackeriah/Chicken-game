0dce5a576c9d24fdc23d660d502f02fb97827701
Owen <fitzgerald.owen@gmail.com>
2020-05-15 13:29:36 +1000

    camera follows player v1

---
+++ Assets/cameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    public float smoothSpeed = 0.125f;
+
+    // offset camera position
+    public Vector3 offset;
+
+    private void LateUpdate()
+    {
+        // follow target and add offset
+        // this is added a vector3 (target position) to a vector3 (camera offset position) so it literally adds the x+x, y+y and z+z
+        transform.position = target.position + offset; 
+    }
+
+}
